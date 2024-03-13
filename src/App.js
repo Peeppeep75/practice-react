d85b47c0a5bbb4990f3a055dbbd3e84d50b889c1
Peeppeep75 <n_n_bsk.75@icloud.com>
2024-03-13 11:50:36 +0900

    memoの使い方

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 import { ChildArea } from "./ChildArea";
 
 export default function App() {
-  const [text, setText] = useState(0);
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = () => setOpen(false);
 
   return (
     <div className="App">
       <br />
-      <input />
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button>表示</button>
-      <ChildArea></ChildArea>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,7 +1,22 @@
-export const ChildArea = () => {
-  return (
-    <div>
-      <p>子コンポーネント</p>
-    </div>
-  );
+import { memo } from "react";
+
+const style = {
+  wight: "100%",
+  height: "200px",
+  backgroundColor: "khaki",
 };
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
